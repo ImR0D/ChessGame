1f06e842b790ef9d3353eb5d5a773ef818113d59
ImR0D <97006482+ImR0D@users.noreply.github.com>
2023-06-14 14:21:26 -0300

    Position deployment

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,6 +2,39 @@ package boardgame;
 
 public class Position {
 	
+	private int row = 0;
+	private int column = 0;
 	
+	public Position() {}
 	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	protected void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	protected void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Position: (x: %02d, y: %02d)", this.row, this.column);
+	}
 }
