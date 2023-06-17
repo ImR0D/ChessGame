bf8ee640328ee321e3ae1cb639ea177239550918
ImR0D <97006482+ImR0D@users.noreply.github.com>
2023-06-17 05:57:34 -0300

    Deployed Chess - Exception and Position

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,5 +1,11 @@
 package chess;
 
-public class ChessException {
+public class ChessException extends RuntimeException {
 
+	private static final long serialVersionUID = 6492993152489147354L;
+	
+	public ChessException(String message) {
+		super(message);
+	}
+	
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,34 @@
 package chess;
 
-public class ChessPosition {
+import boardgame.Position;
 
+public class ChessPosition {
+	
+	private int row;
+	private char column;
+	
+	public int getRow() {
+		return this.row;
+	}
+	public char getColumn() {
+		return this.column;
+	}
+	
+	public ChessPosition(char column, int row) {
+		if ( !(column < 'a' || column > 'h') || (row < 1 || row > 8)) {
+			this.column = column;
+			this.row = row;
+		} else {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from A1 to H8");
+		}
+	}
+	
+	protected Position toPosition() {
+		return null;
+	}
+	
+	protected ChessPosition fromPosition(Position position) {
+		return null;
+	}
+	
 }
