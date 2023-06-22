7891975e393e7e79927d0e9a55d630070184c945
ImR0D <97006482+ImR0D@users.noreply.github.com>
2023-06-22 18:15:19 -0300

    Initial game test deploy

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
+	static Scanner ikeyboard = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());	
+		
+		while (true) {
+			try {
+				UI.printBoard(chessMatch.getPieces());
+				System.out.print("\nSource: ");
+				ChessPosition src = UI.readChessPosition(ikeyboard);
+				System.out.print("\nTarget: ");
+				ChessPosition tgt = UI.readChessPosition(ikeyboard);
+				ChessPiece captured = chessMatch.performChessMove(src, tgt); 
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				ikeyboard.next();
+			}
+		}
 	}
-	
 }
